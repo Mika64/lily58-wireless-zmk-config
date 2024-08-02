2ddeb902ddfe6eb542482c97b34c0da434cc51c3
Michaël Ricart <michael.ricart@eviden.com>
2024-08-02 16:15:30 +0200

    accent fr

---
+++ french-macro.h
@@ -0,0 +1,94 @@
+// é
+ZMK_MACRO(FR_E_ACU_LOW, 
+    bindings = <&kp FR_EACU>, <&to DEF>;
+)
+
+// É
+ZMK_MACRO(FR_E_ACU_UP, 
+    bindings = <&kp CAPSLOCK>, <&kp FR_EACU>, <&kp CAPSLOCK>, <&to DEF>;
+)
+
+// è
+ZMK_MACRO(FR_E_GRV_LOW, 
+    bindings = <&kp FR_EGRV>, <&to DEF>;
+)
+
+// È
+ZMK_MACRO(FR_E_GRV_UP, 
+    bindings = <&kp CAPSLOCK>, <&kp FR_EGRV>, <&kp CAPSLOCK>, <&to DEF>;
+)
+
+// ê
+ZMK_MACRO(FR_E_CIR_LOW, 
+    bindings = <&kp FR_CIRC>, <&kp FR_E>, <&to DEF>;
+)
+// Ê
+ZMK_MACRO(FR_E_CIR_UP, 
+    bindings = <&kp FR_CIRC>, <&kp CAPSLOCK>, <&kp FR_E>, <&kp CAPSLOCK>, <&to DEF>;
+)
+
+// à
+ZMK_MACRO(FR_A_GRV_LOW, 
+    bindings = <&kp FR_AGRV>, <&to DEF>;
+)
+
+// À
+ZMK_MACRO(FR_A_GRV_UP, 
+    bindings = <&kp CAPSLOCK>, <&kp FR_AGRV>, <&kp CAPSLOCK>, <&to DEF>;
+)
+
+// â
+ZMK_MACRO(FR_A_CIR_LOW, 
+    bindings = <&kp FR_CIRC>, <&kp FR_A>, <&to DEF>;
+)
+
+// Â
+ZMK_MACRO(FR_A_CIR_UP, 
+    bindings = <&kp FR_CIRC>, <&kp CAPSLOCK>, <&kp FR_A>, <&kp CAPSLOCK>, <&to DEF>;
+)
+
+// ù
+ZMK_MACRO(FR_U_GRV_LOW, 
+    bindings = <&kp FR_UGRV>, <&to DEF>;
+)
+
+// Ù
+ZMK_MACRO(FR_U_GRV_UP, 
+    bindings = <&kp CAPSLOCK>, <&kp FR_UGRV>, <&kp CAPSLOCK>, <&to DEF>;
+)
+
+// û
+ZMK_MACRO(FR_U_CIR_LOW, 
+    bindings = <&kp FR_CIRC>, <&kp FR_U>, <&to DEF>;
+)
+
+// Û
+ZMK_MACRO(FR_U_CIR_UP, 
+    bindings = <&kp FR_CIRC>, <&kp CAPSLOCK>,<&kp FR_U>, <&kp CAPSLOCK>, <&to DEF>;
+)
+
+// î
+ZMK_MACRO(FR_I_CIR_LOW, 
+    bindings = <&kp FR_CIRC>, <&kp FR_I>, <&to DEF>;
+)
+
+// Î
+ZMK_MACRO(FR_I_CIR_UP, 
+    bindings = <&kp FR_CIRC>, <&kp CAPSLOCK>,<&kp FR_I>, <&kp CAPSLOCK>, <&to DEF>;
+)
+
+// ô
+ZMK_MACRO(FR_O_CIR_LOW, 
+    bindings = <&kp FR_CIRC>, <&kp FR_O>, <&to DEF>;
+)
+
+// Ô
+ZMK_MACRO(FR_O_CIR_UP, 
+    bindings = <&kp FR_CIRC>, <&kp CAPSLOCK>,<&kp FR_O>, <&kp CAPSLOCK>, <&to DEF>;
+)
+
+// ¨
+ZMK_MACRO(FR_TREM, 
+    bindings = <&kp FR_DIAE>, <&to DEF>;
+)
+
---
+++ french-morph.h
@@ -0,0 +1,58 @@
+ZMK_MOD_MORPH(dash_question,
+    bindings = <&kp FR_MINS>, <&kp FR_QUES>;
+    mods = <(MOD_LSFT|MOD_RSFT)>;
+)
+
+ZMK_MOD_MORPH(dot_colon,
+    bindings = <&kp FR_DOT>, <&kp FR_COLN>;
+    mods = <(MOD_LSFT|MOD_RSFT)>;
+)
+
+ZMK_MOD_MORPH(comma_semicolon,
+    bindings = <&kp FR_COMM>, <&kp FR_SCLN>;
+    mods = <(MOD_LSFT|MOD_RSFT)>;
+)
+
+ZMK_MOD_MORPH(FR_E_ACU,
+    bindings = <&FR_E_ACU_LOW>, <&FR_E_ACU_UP>;
+    mods = <(MOD_LSFT|MOD_RSFT)>;
+)
+ZMK_MOD_MORPH(FR_E_GRV,
+    bindings = <&FR_E_GRV_LOW>, <&FR_E_GRV_UP>;
+    mods = <(MOD_LSFT|MOD_RSFT)>;
+)
+ZMK_MOD_MORPH(FR_E_CIR,
+    bindings = <&FR_E_CIR_LOW>, <&FR_E_CIR_UP>;
+    mods = <(MOD_LSFT|MOD_RSFT)>;
+)
+ZMK_MOD_MORPH(FR_A_CIR,
+    bindings = <&FR_A_CIR_LOW>, <&FR_A_CIR_UP>;
+    mods = <(MOD_LSFT|MOD_RSFT)>;
+)
+ZMK_MOD_MORPH(FR_A_GRV,
+    bindings = <&FR_A_GRV_LOW>, <&FR_A_GRV_UP>;
+    mods = <(MOD_LSFT|MOD_RSFT)>;
+)
+ZMK_MOD_MORPH(FR_U_CIR,
+    bindings = <&FR_U_CIR_LOW>, <&FR_U_CIR_UP>;
+    mods = <(MOD_LSFT|MOD_RSFT)>;
+)
+ZMK_MOD_MORPH(FR_U_GRV,
+    bindings = <&FR_U_GRV_LOW>, <&FR_U_GRV_UP>;
+    mods = <(MOD_LSFT|MOD_RSFT)>;
+)
+ZMK_MOD_MORPH(FR_I_CIR,
+    bindings = <&FR_I_CIR_LOW>, <&FR_I_CIR_UP>;
+    mods = <(MOD_LSFT|MOD_RSFT)>;
+)
+ZMK_MOD_MORPH(FR_O_CIR,
+    bindings = <&FR_O_CIR_LOW>, <&FR_O_CIR_UP>;
+    mods = <(MOD_LSFT|MOD_RSFT)>;
+)
+
+
+
+ZMK_MOD_MORPH(dead_key,
+    bindings = <&to 1>, <&kp FR_EXLM>;
+    mods = <(MOD_LSFT|MOD_RSFT)>;
+)
