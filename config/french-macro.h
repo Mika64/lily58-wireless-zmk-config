0d0e5dc86c33579ee202cb255ae254c11672398c
Michaël Ricart <michael.ricart@eviden.com>
2024-08-04 22:06:32 +0200

    fix layout name

--- config/french-macro.h
+++ config/french-macro.h
@@ -1,104 +1,104 @@
 // é
 ZMK_MACRO(FR_E_ACU_LOW, 
-    bindings = <&kp FR_EACU>, <&to DEF>;
+    bindings = <&kp FR_EACU>, <&to ERGOL_FR>;
 )
 
 // É
 ZMK_MACRO(FR_E_ACU_UP, 
-    bindings = <&kp CAPSLOCK>, <&kp FR_EACU>, <&kp CAPSLOCK>, <&to DEF>;
+    bindings = <&kp CAPSLOCK>, <&kp FR_EACU>, <&kp CAPSLOCK>, <&to ERGOL_FR>;
 )
 
 // è
 ZMK_MACRO(FR_E_GRV_LOW, 
-    bindings = <&kp FR_EGRV>, <&to DEF>;
+    bindings = <&kp FR_EGRV>, <&to ERGOL_FR>;
 )
 
 // È
 ZMK_MACRO(FR_E_GRV_UP, 
-    bindings = <&kp CAPSLOCK>, <&kp FR_EGRV>, <&kp CAPSLOCK>, <&to DEF>;
+    bindings = <&kp CAPSLOCK>, <&kp FR_EGRV>, <&kp CAPSLOCK>, <&to ERGOL_FR>;
 )
 
 // ê
 ZMK_MACRO(FR_E_CIR_LOW, 
-    bindings = <&kp FR_CIRC>, <&kp FR_E>, <&to DEF>;
+    bindings = <&kp FR_CIRC>, <&kp FR_E>, <&to ERGOL_FR>;
 )
 // Ê
 ZMK_MACRO(FR_E_CIR_UP, 
-    bindings = <&kp FR_CIRC>, <&kp CAPSLOCK>, <&kp FR_E>, <&kp CAPSLOCK>, <&to DEF>;
+    bindings = <&kp FR_CIRC>, <&kp CAPSLOCK>, <&kp FR_E>, <&kp CAPSLOCK>, <&to ERGOL_FR>;
 )
 
 // à
 ZMK_MACRO(FR_A_GRV_LOW, 
-    bindings = <&kp FR_AGRV>, <&to DEF>;
+    bindings = <&kp FR_AGRV>, <&to ERGOL_FR>;
 )
 
 // À
 ZMK_MACRO(FR_A_GRV_UP, 
-    bindings = <&kp CAPSLOCK>, <&kp FR_AGRV>, <&kp CAPSLOCK>, <&to DEF>;
+    bindings = <&kp CAPSLOCK>, <&kp FR_AGRV>, <&kp CAPSLOCK>, <&to ERGOL_FR>;
 )
 
 // â
 ZMK_MACRO(FR_A_CIR_LOW, 
-    bindings = <&kp FR_CIRC>, <&kp FR_A>, <&to DEF>;
+    bindings = <&kp FR_CIRC>, <&kp FR_A>, <&to ERGOL_FR>;
 )
 
 // Â
 ZMK_MACRO(FR_A_CIR_UP, 
-    bindings = <&kp FR_CIRC>, <&kp CAPSLOCK>, <&kp FR_A>, <&kp CAPSLOCK>, <&to DEF>;
+    bindings = <&kp FR_CIRC>, <&kp CAPSLOCK>, <&kp FR_A>, <&kp CAPSLOCK>, <&to ERGOL_FR>;
 )
 
 // ù
 ZMK_MACRO(FR_U_GRV_LOW, 
-    bindings = <&kp FR_UGRV>, <&to DEF>;
+    bindings = <&kp FR_UGRV>, <&to ERGOL_FR>;
 )
 
 // Ù
 ZMK_MACRO(FR_U_GRV_UP, 
-    bindings = <&kp CAPSLOCK>, <&kp FR_UGRV>, <&kp CAPSLOCK>, <&to DEF>;
+    bindings = <&kp CAPSLOCK>, <&kp FR_UGRV>, <&kp CAPSLOCK>, <&to ERGOL_FR>;
 )
 
 // û
 ZMK_MACRO(FR_U_CIR_LOW, 
-    bindings = <&kp FR_CIRC>, <&kp FR_U>, <&to DEF>;
+    bindings = <&kp FR_CIRC>, <&kp FR_U>, <&to ERGOL_FR>;
 )
 
 // Û
 ZMK_MACRO(FR_U_CIR_UP, 
-    bindings = <&kp FR_CIRC>, <&kp CAPSLOCK>, <&kp FR_U>, <&kp CAPSLOCK>, <&to DEF>;
+    bindings = <&kp FR_CIRC>, <&kp CAPSLOCK>, <&kp FR_U>, <&kp CAPSLOCK>, <&to ERGOL_FR>;
 )
 
 // î
 ZMK_MACRO(FR_I_CIR_LOW, 
-    bindings = <&kp FR_CIRC>, <&kp FR_I>, <&to DEF>;
+    bindings = <&kp FR_CIRC>, <&kp FR_I>, <&to ERGOL_FR>;
 )
 
 // Î
 ZMK_MACRO(FR_I_CIR_UP, 
-    bindings = <&kp FR_CIRC>, <&kp CAPSLOCK>, <&kp FR_I>, <&kp CAPSLOCK>, <&to DEF>;
+    bindings = <&kp FR_CIRC>, <&kp CAPSLOCK>, <&kp FR_I>, <&kp CAPSLOCK>, <&to ERGOL_FR>;
 )
 
 // ô
 ZMK_MACRO(FR_O_CIR_LOW, 
-    bindings = <&kp FR_CIRC>, <&kp FR_O>, <&to DEF>;
+    bindings = <&kp FR_CIRC>, <&kp FR_O>, <&to ERGOL_FR>;
 )
 
 // Ô
 ZMK_MACRO(FR_O_CIR_UP, 
-    bindings = <&kp FR_CIRC>, <&kp CAPSLOCK>, <&kp FR_O>, <&kp CAPSLOCK>, <&to DEF>;
+    bindings = <&kp FR_CIRC>, <&kp CAPSLOCK>, <&kp FR_O>, <&kp CAPSLOCK>, <&to ERGOL_FR>;
 )
 
 // ¨
 ZMK_MACRO(FR_TREM, 
-    bindings = <&kp FR_DIAE>, <&to DEF>;
+    bindings = <&kp FR_DIAE>, <&to ERGOL_FR>;
 )
 
 // ç
-//ZMK_MACRO(FR_C_CED_LOW, 
-//    bindings = <&kp FR_CCED>, <&kp FR_O>, <&to DEF>;
-//)
+ZMK_MACRO(FR_C_CED_LOW, 
+    bindings = <&kp FR_CCED>, <&kp FR_O>, <&to ERGOL_FR>;
+)
 
 // Ç
-//ZMK_MACRO(FR_C_CED_UP, 
-//    bindings = <&kp FR_CCED>, <&kp CAPSLOCK>, <&kp FR_O>, <&kp CAPSLOCK>, <&to DEF>;
-//)
+ZMK_MACRO(FR_C_CED_UP, 
+    bindings = <&kp FR_CCED>, <&kp CAPSLOCK>, <&kp FR_O>, <&kp CAPSLOCK>, <&to ERGOL_FR>;
+)
 
